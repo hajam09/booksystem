3bc96c9b141c4019aab7a4b31f9e4b63e6b647f7
hajam09 <hajam09@yahoo.com>
2020-02-03 08:57:20 +0000

    user like/dislike post on creation fixed

--- mainapp/views.py
+++ mainapp/views.py
@@ -620,7 +620,7 @@ def book_page(request, isbn_13):
 
 	#Set of books to display for suggestions.
 	#item_based_recommendation = get_item_based_recommendation(csv_file)
-	# average_rating_recommendation = weighted_average_and_favourite_score(request)# Not sure if this is used in book.html
+	average_rating_recommendation = weighted_average_and_favourite_score(request)# Not sure if this is used in book.html
 	pearson_correlation_collaborative_filtering(request)
 
 	#Need to get all the reviews associated with the book.
