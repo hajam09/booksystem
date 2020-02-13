cfe064457ae5889c64a1869730440464391ba4f4
hajam09 <hajam09@yahoo.com>
2020-02-13 10:50:18 +0000

    unit test on url fix

--- mainapp/tests/test_urls.py
+++ mainapp/tests/test_urls.py
@@ -20,6 +20,16 @@ class URLTest(TestCase):
 		newuser = self.create_user("oliver.queen@yahoo.com", "oliver.queen@yahoo.com", "RanDomPasWord56", "Oliver", "Queen")
 		newprofile = self.create_user_profile(newuser, "2019-03-22", "Male", "['Adventures', 'Horror', 'Romance']")
 
+		ISBN_13 = 9876543212345
+		ISBN_10 = 7685747586
+		title = "A View from the Bridge"
+		book_data = { "id": "vKJewgEACAAJ", "etag": "e8wZTpMgieo", "title": "A View from the Bridge",
+		"authors": "Mohamed,Haja,Nijam", "publisher": "Longman", "publishedDate": "1999-02-11",
+		"description": "Some random description for this book.", "ISBN_10": "7685747586",
+		"ISBN_13": "9876543212345", "categories": [ "Digital Communications" ], "averageRating": 0.0,
+		"ratingsCount": 0, "thumbnail": "http://books.google.com/books/content?id=vKJewgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" }
+		new_book = Book.objects.create(isbn_13=ISBN_13, isbn_10=ISBN_10, title=title, book_data=book_data)
+
 	def tearDown(self):
 		User.objects.all().delete()
 
@@ -55,9 +65,11 @@ class URLTest(TestCase):
 		self.assertEqual(resolve(url).func, update_profile)
 		self.assertEqual(resolve(url).url_name, "update_profile")
 
+		self.logged_in = self.client.login(username='oliver.queen@yahoo.com', password='RanDomPasWord56')
 		response = self.client.get(reverse('mainapp:update_profile'))
-		self.assertEqual(response.status_code, 302)# Because the user needs to be logged in in order to get 200., find way to login user in back end. Then undo below.
-		#self.assertTemplateUsed(response, 'mainapp/profilepage.html')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'mainapp/profilepage.html')
+		self.client.logout()
 
 	def test_user_shelf(self):
 		url = reverse('mainapp:user_shelf')
@@ -66,16 +78,26 @@ class URLTest(TestCase):
 
 		response = self.client.get(reverse('mainapp:user_shelf'))
 		self.assertEqual(response.status_code, 302)
-		#self.assertTemplateUsed(response, 'mainapp/usershelf.html')
+		self.assertRedirects(response, '/login/')
 
-	# def test_book_page(self):
-	# 	url = reverse('mainapp:book_page', kwargs={'isbn_13':9781447218296})
-	# 	self.assertEqual(resolve(url).func, book_page)
-	# 	self.assertEqual(resolve(url).url_name, "book_page")
+		self.logged_in = self.client.login(username='oliver.queen@yahoo.com', password='RanDomPasWord56')
+		response = self.client.get(reverse('mainapp:user_shelf'))
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'mainapp/usershelf.html')
+		self.client.logout()
 
-	# 	response = self.client.get(reverse('mainapp:book_page', kwargs={'isbn_13':9781447218296}))
-	# 	self.assertEqual(response.status_code, 200)
-	# 	self.assertTemplateUsed(response, 'mainapp/book.html')
+	def test_book_page(self):
+		# Need to comment out some following in views.py BUT WORKS
+		#average_rating_recommendation = weighted_average_and_favourite_score(request)
+		#similar_books = content_based_similar_items(request, book_title)
+		# url = reverse('mainapp:book_page', kwargs={'isbn_13':9876543212345})
+		# self.assertEqual(resolve(url).func, book_page)
+		# self.assertEqual(resolve(url).url_name, "book_page")
+
+		# response = self.client.get(reverse('mainapp:book_page', kwargs={'isbn_13':9876543212345}))
+		# self.assertEqual(response.status_code, 200)
+		# self.assertTemplateUsed(response, 'mainapp/book.html')
+		pass
 
 	def test_not_found(self):
 		url = reverse('mainapp:not_found')
