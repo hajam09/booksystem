1de2de68c86b45b4703ab3bce99dfd354858e0b8
hajam09 <hajam09@yahoo.com>
2020-02-20 09:45:43 +0000

    tests commented

---
+++ mainapp/tests/test_requirements.py
@@ -0,0 +1,15 @@
+# from django.test import TestCase, Client, RequestFactory, LiveServerTestCase
+# from selenium.webdriver.common.keys import Keys
+# import json, time
+# from bksys.models import *
+# from django.contrib.sessions.middleware import SessionMiddleware
+# from selenium import webdriver
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.common.exceptions import TimeoutException, WebDriverException
+# from django.conf import settings
+# from django.contrib.auth.models import User
+# from selenium.webdriver.support.ui import Select
+# # python manage.py test mainapp/tests
+
+# class ErrorPageTest(LiveServerTestCase):
